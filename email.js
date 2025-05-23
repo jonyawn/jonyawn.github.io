22c77e64034016f72a8316b21064aafa920292ff
jonyawn <jonyawn@gmail.com>
2025-05-23 16:56:12 -0400

    Moved code over from other website

---
+++ email.js
@@ -0,0 +1,12 @@
+function getData(form) {
+    var formData = new FormData(form);
+
+for (var pair of formData.entries()) {
+    console.log(pair[0] + ": " + pair[1]);
+    console.log(Object.fromEntries(formData));
+}
+}
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+   getData(e.target)
+});
